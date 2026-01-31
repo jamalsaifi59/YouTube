e9bd18cdff5e8b665f80a38f0f171449caa58127
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-01-31 16:50:25 +0530

    remove console

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,12 +6,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,        
     api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
-console.log("Cloudinary ENV check:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
 
 const UploadOnCloudinary = async (localFilepath) => {
     try {
